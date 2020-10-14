2c231b12789942a41a6d295b85e5c1246744b9fa
hanson377 <hanson377@gmail.com>
2020-10-14 06:37:25 -0600

    updating some things

--- hansPacks/R/bayes_prop_ab.R
+++ hansPacks/R/bayes_prop_ab.R
@@ -1,3 +1,12 @@
+#' A Bayes probability calculation from two proportions
+#'
+#' This functions allows you to quickly calculate the probability that one proportion is greater than another
+#' @param love Do you love cats? Defaults to TRUE.
+#' @keywords bayes, ab test, proportion
+#' @export
+#' @examples
+#' bayes_prop_ab()
+
 bayes_prop_ab <- function(prior_alpha,prior_beta,p1,p2,n1,n2,trials) {
 alpha1 <- n1*p1
 alpha2 <- n2*p2
